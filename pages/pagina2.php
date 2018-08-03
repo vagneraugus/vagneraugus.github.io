5b4dc0e58c5762fbff523916d647d89fd96df4cd
vagnereu <vagneraugustinho@gmail.com>
2018-08-03 16:33:29 -0300

    Atualização bloco competencias

--- pages/pagina2.php
+++
@@ -1,1 +0,0 @@
-<h1>Pagina kkkk</h1>
---
+++ style/js/jsManual.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+$(document).ready(function(){
+    
+
+    // ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬ Começo Botão topo ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+    $(function(){
+ 
+        $(document).on( 'scroll', function(){
+     
+            if ($(window).scrollTop() > 100) {
+                $('.btn_topo').addClass('show');
+            } else {
+                $('.btn_topo').removeClass('show');
+            }
+        });
+     
+        $('.btn_topo').on('click', scrollToTop);
+    });
+     
+    function scrollToTop() {
+        verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+        element = $('body');
+        offset = element.offset();
+        offsetTop = offset.top;
+        $('html, body').animate({scrollTop: offsetTop}, 2300, 'linear');
+    }
+    // ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬ Fim Botão topo ¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+});
